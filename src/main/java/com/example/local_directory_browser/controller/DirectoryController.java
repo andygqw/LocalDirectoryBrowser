b95d593bc44538c69cb98f7ba248ac13847e1a18
andygqw <andygqwang@gmail.com>
2024-09-09 18:26:06 -0700

    globa lexception handling

--- src/main/java/com/example/local_directory_browser/controller/DirectoryController.java
+++ src/main/java/com/example/local_directory_browser/controller/DirectoryController.java
@@ -11,7 +11,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.FileNotFoundException;
-import java.util.List;
 
 @RestController
 @RequestMapping(value="/browse")
--- src/main/java/com/example/local_directory_browser/service/DirectoryService.java
+++ src/main/java/com/example/local_directory_browser/service/DirectoryService.java
@@ -5,8 +5,6 @@ import org.springframework.stereotype.Service;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.List;
 
 @Service
 public class DirectoryService {
