7d92c0d16dbc0d392737471897ac3cffa96dcec2
andygqw <andygqwang@gmail.com>
2024-09-09 19:51:44 -0700

    disabled CORS

--- src/main/java/com/example/local_directory_browser/controller/DirectoryController.java
+++ src/main/java/com/example/local_directory_browser/controller/DirectoryController.java
@@ -9,11 +9,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import java.io.FileNotFoundException;
 
 @RestController
 @RequestMapping(value="/browse")
+@CrossOrigin(origins = "*")
 public class DirectoryController {
 
     private final DirectoryService directoryService;
