ffb5f4c5cad9e5d7278c5568e27f0144ba334121
andygqw <andygqwang@gmail.com>
2024-09-09 18:51:40 -0700

    allowed extensions

--- src/main/java/com/example/local_directory_browser/service/DirectoryService.java
+++ src/main/java/com/example/local_directory_browser/service/DirectoryService.java
@@ -5,11 +5,14 @@ import org.springframework.stereotype.Service;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.List;
 
 @Service
 public class DirectoryService {
 
     private final static String BASE = "";
+    private final static List<String> ALLOWED_FILES = Arrays.asList(".mp4", ".mov", ".pdf", ".jpg", ".jpeg", "png", "mp3");
 
     public DirectoryDTO listFilesRecursively(String directoryPath) throws FileNotFoundException {
         DirectoryDTO files = new DirectoryDTO(directoryPath);
@@ -28,7 +31,7 @@ public class DirectoryService {
 
         if (fileList != null) {
             for (File file : fileList) {
-                if (file.isFile()) {
+                if (file.isFile() && ALLOWED_FILES.contains(normalizeExtension(file.getName()))) {
                     files.addFile(file.getName(), file.getAbsolutePath());
                 } else if (file.isDirectory()) {
                     files.addDirectory(file.getAbsolutePath());
@@ -37,5 +40,9 @@ public class DirectoryService {
         } else {
             throw new FileNotFoundException("File.listFiles(): " + directory.getAbsolutePath());
         }
+    }
+
+    private String normalizeExtension(String fileName) {
+        return fileName.substring(fileName.lastIndexOf(".")).toLowerCase();
     }
 }
