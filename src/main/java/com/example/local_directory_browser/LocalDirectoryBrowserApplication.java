5bb4fd3c0d6fa78e8584388851111d1524a29764
andygqw <andygqwang@gmail.com>
2024-09-09 17:43:56 -0700

    first commit

---
+++ src/main/java/com/example/local_directory_browser/LocalDirectoryBrowserApplication.java
@@ -0,0 +1,13 @@
+package com.example.local_directory_browser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocalDirectoryBrowserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocalDirectoryBrowserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/local_directory_browser/LocalDirectoryBrowserApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.local_directory_browser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocalDirectoryBrowserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
